c50dd8dd760755fdc69872f9a1264de4d5ba7b44
EihabAhmed <eihabahmed@gmail.com>
2024-03-11 16:30:55 +0200

    Added url of Heroku

--- GameNodeJs/SocketHandler.swift
+++ GameNodeJs/SocketHandler.swift
@@ -10,7 +10,9 @@ import SocketIO
 
 class SocketHandler: NSObject {
     static let sharedInstance = SocketHandler()
-    let socket = SocketManager(socketURL: URL(string: "http://192.168.1.7:3000")!, config: [.log(true), .compress])
+    let socket = SocketManager(socketURL: URL(string: "https://game-node-js-5412cbf208fc.herokuapp.com")!, config: [.log(true), .compress])
+//    let socket = SocketManager(socketURL: URL(string: "http://192.168.1.7:3000")!, config: [.log(true), .compress])
+//    let socket = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.log(true), .compress])
     var mSocket: SocketIOClient!
     
     override init() {
